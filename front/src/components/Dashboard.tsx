8f855d126142a8921638471c932703061955746a
Clément AKOUÈGNON ABIKOU <clement.clement@chem.loc>
2025-01-21 10:19:13 +0100

    dashboard amelioré et le boutton de deconnexion psitionné au coin superieur à droite

--- front/src/components/Dashboard.tsx
+++ front/src/components/Dashboard.tsx
@@ -1,37 +1,28 @@
 import React from "react";
-import {
-    Container,
-    CssBaseline,
-    Box,
-    Typography,
-    Button,
-    Grid,
-} from "@mui/material";
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Dashboard = () => {
     const navigate = useNavigate();
 
     const handleLogout = async () => {
-        const token = localStorage.getItem('token'); // Récupérer le token
+        const token = localStorage.getItem('token');
 
         if (!token) {
             console.error("Token is null or not found");
-            return; // Arrêtez la fonction si le token est absent
+            return;
         }
         try {
             const response = await axios.post("http://127.0.0.1:8000/logout", {}, {
                 headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`, // Vérifiez que le token n'est pas null
+                    'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json',
                 },
             });
 
             if (response.status === 200) {
-                // Supprimez le token localement ou toute autre donnée d'authentification
                 localStorage.removeItem('token');
-                navigate('/login'); // Redirigez vers la page de connexion
+                navigate('/login');
             } else {
                 console.error("Erreur lors de la déconnexion", response.statusText);
             }
@@ -41,39 +32,18 @@ const Dashboard = () => {
     };
 
     return (
-        <>
-            <Container maxWidth="lg">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        mt: 10,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography variant="h2" gutterBottom>
-                        Bienvenue sur votre tableau de bord !
-                    </Typography>
-                    <Typography variant="h5" align="center" sx={{ mb: 4 }}>
-                        Vous êtes connecté. Profitez de votre expérience.
-                    </Typography>
-
-                    <Grid container spacing={5} sx={{ mt: 5 }}>
-                    
-                            <Button
-                                onClick={handleLogout}
-                                fullWidth
-                                variant="outlined"
-                            >
-                                Déconnexion
-                            </Button>
-                
-                    </Grid>
-                </Box>
-            </Container>
-        </>
+        <div className="relative flex flex-col items-center justify-center min-h-screen">
+            <button
+                onClick={handleLogout}
+                className="absolute top-4 right-4 border bg-blue-600 border-gray-300 text-white hover:bg-red-600 rounded-lg py-2 px-4"
+            >
+                Déconnexion
+            </button>
+            <div className="text-center p-8">
+                <h2 className="text-4xl font-bold mb-4">Bienvenue sur votre tableau de bord !</h2>
+                <h5 className="text-lg mb-6">Vous êtes connecté. Profitez de votre expérience.</h5>
+            </div>
+        </div>
     );
 };
-
 export default Dashboard;
